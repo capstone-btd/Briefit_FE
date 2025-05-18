96b096d5fc19461b826470d27bc7d5caf6f20e15
sunjae0902 <sunjaegg0902@naver.com>
2025-05-18 22:35:12 +0900

    test: 네이버 로그인

---
+++ src/utils/axios.ts
@@ -0,0 +1,4 @@
+import axios from "axios";
+
+// axios 요청/응답 인터셉터 정의
+
---
+++ types/auth/authState.ts
@@ -0,0 +1,13 @@
+import { User } from "./user";
+
+// 유저 상태 모델
+
+export type AuthState = {
+  isLoggedIn: boolean;
+  user: User | null;
+  token: string | null; // 추후 수정 필요
+  setUser: (user: User) => void;
+  setToken: (token: string) => void; // 추후 수정 필요
+  login: (user: User, token: string) => void; // 추후 수정 필요
+  logout: () => void;
+};
