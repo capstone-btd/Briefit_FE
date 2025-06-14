f248d7d43a0e22bd5057bf66f6459b92273dfc4a
sunjae0902 <sunjaegg0902@naver.com>
2025-06-15 01:02:16 +0900

    fix: prettier 설정 수정

--- prettier.config.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-// prettier.config.ts
-export default {
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindStylesheet: "./src/app/globals.css",
-};
