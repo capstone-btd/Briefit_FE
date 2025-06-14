6548c7a955d052511006255e6263018acf6e19da
sunjae0902 <sunjaegg0902@naver.com>
2025-06-15 01:38:38 +0900

    update: 검색 바에 아이콘 추가

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,10 +1,28 @@
-export default function Searchbar() {
+"use client";
+
+import IconButton from "@/features/common/IconButton";
+
+type SearchbarProps = {
+  className?: string;
+  searchIconSize?: number;
+};
+
+export default function Searchbar({
+  className = "",
+  searchIconSize = 27,
+}: SearchbarProps) {
   return (
-    <div className="aspect-[277/50] w-full max-w-277">
-      <div className="input-gradient-border h-full w-full from-purple-500 to-green-500">
+    <div className={`relative ${className}`}>
+      <div className="input-gradient-border relative h-full w-full from-purple-500 to-green-500">
         <input
-          className="input-inner font-basic-16 placeholder-gray-200"
+          className="input-inner h-full w-full pr-12 font-basic-16 placeholder-gray-200"
           placeholder="검색어를 입력하세요."
+        />
+        <IconButton
+          iconName="search"
+          className="absolute top-1/2 right-16 -translate-y-1/2"
+          onClick={() => {}}
+          style={{ width: searchIconSize, height: searchIconSize }}
         />
       </div>
     </div>
