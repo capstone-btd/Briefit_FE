32e829521f03d140dc5a9799972212bb19be2424
sunjae0902 <sunjaegg0902@naver.com>
2025-06-16 03:35:16 +0900

    update: 검색 api 연동

---
+++ src/features/search/api/search.ts
@@ -0,0 +1,26 @@
+import ApiException from "@/exception/apiException";
+import apiServer from "@/utils/apiServer";
+
+export default async function fetchNewsCardListByKeyword({
+  keyword
+}: {
+  keyword: string;
+}) {
+  const params = { string: keyword };
+  try {
+    const response = await apiServer.get("/articles/search", {
+      params,
+      headers: {
+        "x-auth-not-required": "true", // 인증 헤더 제외
+      },
+    });
+    return response.data;
+  } catch (error) {
+    if (error instanceof ApiException) {
+      // 예외 처리
+    } else {
+      console.error(error);
+    }
+    return;
+  }
+}
--- src/features/search/components/SearchResult.tsx
+++ src/features/search/components/SearchResult.tsx
@@ -2,13 +2,16 @@ import Searchbar from "@/components/Searchbar";
 import { DetailPageType } from "@/constants/detailPageType";
 import Divider from "@/features/common/Divider";
 import PaginatedNewsCardGrid from "@/features/common/PaginatedNewsCardGrid";
-import { dummyNews } from "@/mock/dummyNews";
+import fetchNewsCardListByKeyword from "../api/search";
+import NoContent from "@/features/common/NoContent";
 
 type SearchProps = {
   keyword: string;
 };
 
-export default function SearchResult({ keyword }: SearchProps) {
+export default async function SearchResult({ keyword }: SearchProps) {
+  const newsList = await fetchNewsCardListByKeyword({ keyword: keyword });
+
   return (
     <div>
       <Searchbar
@@ -21,12 +24,13 @@ export default function SearchResult({ keyword }: SearchProps) {
       </div>
       <Divider />
       <div className="mx-20 mt-60">
-        <PaginatedNewsCardGrid
-          itemsPerPage={6}
-          newsList={dummyNews}
-          categoryLabel={null}
-          type={DetailPageType.TODAY}
-        />
+        {newsList.length === 0 ? <NoContent message="검색 결과가 없어요."/> :
+          <PaginatedNewsCardGrid
+            itemsPerPage={6}
+            newsList={newsList}
+            categoryLabel={null}
+            type={DetailPageType.TODAY}
+          />}
       </div>
     </div>
   );
