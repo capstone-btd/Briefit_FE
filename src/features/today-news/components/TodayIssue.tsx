52bea9e0fb8fa1817e3c6e22fe1bf8406e59b4cd
sunjae0902 <sunjaegg0902@naver.com>
2025-06-18 17:30:57 +0900

    fix: 워드클라우드 예외처리 수정

--- src/features/today-news/components/TodayIssue.tsx
+++ src/features/today-news/components/TodayIssue.tsx
@@ -4,27 +4,34 @@ import WordCloudServer from "./WordCloudServer";
 import NoContent from "@/features/common/NoContent";
 
 export default async function TodayIssue() {
-  const wordCloudList = await fetchWordList();
-  const createdAt = wordCloudList.createdAt;
+  const wordCloudData = await fetchWordList();
+
+  if (!wordCloudData) {
+    return (
+      <div className="space-y-7">
+        <div className="flex items-center space-x-50">
+          <div className="font-title-24">오늘의 이슈</div>
+        </div>
+        <NoContent message="불러올 데이터가 없어요." />
+      </div>
+    );
+  }
+
+  const { createdAt } = wordCloudData;
+
   return (
     <div className="space-y-7">
       <div className="flex items-center space-x-50">
         <div className="font-title-24">오늘의 이슈</div>
-        {!Array.isArray(wordCloudList) || wordCloudList.length === 0 ? (
-          <NoContent message="불러올 데이터가 없어요." />
-        ) : (
-          <div>
-            <div className="font-basic-16 text-gray-400">
-              {formatKoreanDateTime({
-                isoString: createdAt,
-                needUnitText: true,
-              })}{" "}
-              기준
-            </div>
-            <WordCloudServer wordCloudData={wordCloudList} />
-          </div>
-        )}
+        <div className="font-basic-16 text-gray-400">
+          {formatKoreanDateTime({
+            isoString: createdAt,
+            needUnitText: true,
+          })}{" "}
+          기준
+        </div>
       </div>
+      <WordCloudServer wordCloudData={wordCloudData} />
     </div>
   );
 }
