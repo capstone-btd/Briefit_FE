486a8dd97e6673fa58551231c5adcba0238ea7e6
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2025-10-09 17:00:17 +0900

    fix

--- src/app/today-news/[[...category]]/page.tsx
+++ src/app/today-news/[[...category]]/page.tsx
@@ -1,5 +1,6 @@
 import RefreshOnBackWrapper from "@/components/RefreshOnBackWrapper";
 import { newsCategories } from "@/constants/newsCategries";
+import NewsCategorybar from "@/features/common/categorybar/NewsCategorybar";
 import PressCompanyFilterWrapper from "@/features/common/PressCompanyFilterWrapper";
 import TodayIssue from "@/features/today-news/components/TodayIssue";
 import TodayNewsCardGrid from "@/features/today-news/components/TodayNewsCardGrid";
@@ -24,11 +25,14 @@ export default function TodayNewsPage(props: Props) {
   return (
     <div>
       <div className="flex items-center justify-between">
-        <div className="flex items-center space-x-50">
-          <div className="font-title-24 sm:hidden">오늘의 AI 뉴스</div>
+        <div className="flex items-center space-x-50 sm:hidden">
+          <div className="font-title-24">오늘의 AI 뉴스</div>
+          <NewsCategorybar basePath="today-news" />
         </div>
         {/* TODO: - 수정 필요 */}
-        <div className="sm:hidden"><PressCompanyFilterWrapper /></div> 
+        <div className="sm:hidden">
+          <PressCompanyFilterWrapper />
+        </div>
       </div>
       <div>
         <RefreshOnBackWrapper>
